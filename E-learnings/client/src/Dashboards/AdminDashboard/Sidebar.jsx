18d3eefbd30d7a1097420ebb931fff03e2193da0
abishek <avineshmtp@gmail.com>
2024-11-19 22:02:30 +0530

    third commit

--- E-learnings/client/src/Dashboards/AdminDashboard/Sidebar.jsx
+++ E-learnings/client/src/Dashboards/AdminDashboard/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdFindInPage } from "react-icons/md";
 import { FaHome } from "react-icons/fa";
@@ -6,27 +6,124 @@ import { FaUserCircle } from "react-icons/fa";
 import { FaTableCells } from "react-icons/fa6";
 import { IoPersonAddSharp } from "react-icons/io5";
 import { BiSolidReport } from "react-icons/bi";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 
-const Sidebar = ({isOpen}) => {
-    return(
-        <aside className={`sidebar ${isOpen ? 'open' : ''}`}>
-            <nav>
-                <ul>
-                    <Link to="/admindashboard"><li><FaHome  style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Home</li></Link>
-                    <Link to="/admindashboard/addteacher"><li><IoPersonAddSharp  style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Add Teacher</li></Link>
-                    <Link to="/admindashboard/admintracking"><li><MdFindInPage  style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Admin Tracking</li></Link>
-                    <Link to="/admindashboard/attendancetracking"><li><MdFindInPage style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Attendance Tracking</li></Link>
-                    <Link to="/admindashboard/schoolteachers"><li><BiSolidReport  style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Teachers Report</li></Link>
-                    <Link to="/admindashboard/schoolstudents"><li><BiSolidReport  style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Students Report</li></Link>
-                    <Link to="/admindashboard/addtimetable"><li><FaTableCells  style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Add Time Table</li></Link>
-                    <Link to="/admindashboard/timetabletracking"><li><FaTableCells  style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Time Table Tracking</li></Link> 
-                    <Link to="/admindashboard/profile"><li><FaUserCircle  style={{ fontSize: '21px', marginLeft: '10px' }}/> &nbsp;Profile</li></Link>
-                    
+const Sidebar = ({ isOpen }) => {
+  const [isAddDropdownOpen, setIsAddDropdownOpen] = useState(false);
+  const [isTrackDropdownOpen, setIsTrackDropdownOpen] = useState(false);
+  const [isReportDropdownOpen, setIsReportDropdownOpen] = useState(false);
 
-                </ul>
-            </nav>
-        </aside>
-    )
-}
+  const toggleAddDropdown = () => setIsAddDropdownOpen((prev) => !prev);
+  const toggleTrackDropdown = () => setIsTrackDropdownOpen((prev) => !prev);
+  const toggleReportDropdown = () => setIsReportDropdownOpen((prev) => !prev);
+
+  return (
+    <aside className={`sidebar ${isOpen ? "open" : ""}`}>
+      <nav>
+        <ul>
+          <Link to="/admindashboard">
+            <li>
+              <FaHome style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Home
+            </li>
+          </Link>
+
+          {/* ADD Dropdown */}
+          <li
+            onClick={toggleAddDropdown}
+            style={{ cursor: "pointer", display: "flex", alignItems: "center", color: "white" }}
+          >
+            <IoPersonAddSharp  style={{ fontSize: "21px", marginLeft: "10px", color: "white" }} /> &nbsp;ADD
+            {isAddDropdownOpen ? (
+              <ExpandLessIcon style={{ fontSize: "21px", marginLeft: "70px" }} />
+            ) : (
+              <ExpandMoreIcon style={{ fontSize: "21px", marginLeft: "70px" }} />
+            )}
+          </li>
+          {isAddDropdownOpen && (
+            <ul className="dropdown">
+              <Link to="/admindashboard/addteacher">
+                <li>
+                  <IoPersonAddSharp style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Add Teacher
+                </li>
+              </Link>
+              <Link to="/admindashboard/addtimetable">
+                <li>
+                  <FaTableCells style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Add Time Table
+                </li>
+              </Link>
+            </ul>
+          )}
+
+          {/* Tracking Dropdown */}
+          <li
+            onClick={toggleTrackDropdown}
+            style={{ cursor: "pointer", display: "flex", alignItems: "center", color: "white" }}
+          >
+            <MdFindInPage style={{ fontSize: "21px", marginLeft: "10px", color: "white" }} /> &nbsp;Tracking
+            {isTrackDropdownOpen ? (
+              <ExpandLessIcon style={{ fontSize: "21px", marginLeft: "70px" }} />
+            ) : (
+              <ExpandMoreIcon style={{ fontSize: "21px", marginLeft: "70px" }} />
+            )}
+          </li>
+          {isTrackDropdownOpen && (
+            <ul className="dropdown">
+              <Link to="/admindashboard/admintracking">
+                <li>
+                  <MdFindInPage style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Admin Tracking
+                </li>
+              </Link>
+              <Link to="/admindashboard/attendancetracking">
+                <li>
+                  <MdFindInPage style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Attendance Tracking
+                </li>
+              </Link>
+            </ul>
+          )}
+
+          {/* Reports Dropdown */}
+          <li
+            onClick={toggleReportDropdown}
+            style={{ cursor: "pointer", display: "flex", alignItems: "center", color: "white" }}
+          >
+            <BiSolidReport style={{ fontSize: "21px", marginLeft: "10px", color: "white" }} /> &nbsp;Reports
+            {isReportDropdownOpen ? (
+              <ExpandLessIcon style={{ fontSize: "21px", marginLeft: "70px" }} />
+            ) : (
+              <ExpandMoreIcon style={{ fontSize: "21px", marginLeft: "70px" }} />
+            )}
+          </li>
+          {isReportDropdownOpen && (
+            <ul className="dropdown">
+              <Link to="/admindashboard/schoolteachers">
+                <li>
+                  <BiSolidReport style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Teachers Report
+                </li>
+              </Link>
+              <Link to="/admindashboard/schoolstudents">
+                <li>
+                  <BiSolidReport style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Students Report
+                </li>
+              </Link>
+            </ul>
+          )}
+
+          {/* Other Static Links */}
+          <Link to="/admindashboard/timetabletracking">
+            <li>
+              <FaTableCells style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Time Table Tracking
+            </li>
+          </Link>
+          <Link to="/admindashboard/profile">
+            <li>
+              <FaUserCircle style={{ fontSize: "21px", marginLeft: "10px" }} /> &nbsp;Profile
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
 
 export default Sidebar;
